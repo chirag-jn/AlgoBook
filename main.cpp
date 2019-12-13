dbb6a13fa8f97f6b1083bf9601bcb880c6086fb6
Chirag Jain <jainchg@gmail.com>
2019-12-14 04:48:44 +0530

    Added error to gitignore, added basic code for running scripts

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void setEnv() {
+	#ifndef ONLINE_JUDGE
+    // for getting input from input.txt
+    freopen("input.txt", "r", stdin);
+    // for writing output to output.txt
+    freopen("output.txt", "w", stdout);
+    // for writing error to error.txt
+    freopen("error.txt", "w", stderr);
+	#endif
+}
+
+int main() {
+	setEnv();
+
+	int x;
+	cin>>x;
+	cout<<x<<endl;
+	cerr<<x<<endl;
+
+	return 0;
+}
